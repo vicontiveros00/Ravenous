376b14da40dfe961d44796d6a7a6234f773bd927
Vic <vontiverosewhs@gmail.com>
2022-09-19 13:32:48 +0300

    First commit, a lot of stuff hardcoded but Yelp API will be added soon

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import logo from '../../logo.svg';
+import './App.css';
+import React from 'react';
+import BusinessList from '../BusinessList/BusinessList'
+import SearchBar from '../SearchBar/SearchBar'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  );
+}
+
+export default App;
